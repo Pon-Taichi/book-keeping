f7367ba43ceca63c10a6ccfea8bb9bbba7cb0fe9
Pon-Taichi <saramanda164@gmail.com>
2023-11-21 00:15:53 +0900

    fix env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: true,
     subabaseUrl: 'https://iwjaoikvngyycijlcdio.supabase.co',
     anonkey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3amFvaWt2bmd5eWNpamxjZGlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAwNjMyMTcsImV4cCI6MjAxNTYzOTIxN30.49EaKjqwooR8KHQ90sz3SbEPWB1LCIx9KdSt0no1wPE',
