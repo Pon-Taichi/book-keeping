92bb2bb3f50e534a3ac20f351cec855b467f3893
Pon-Taichi <saramanda164@gmail.com>
2023-11-21 00:20:20 +0900

    rm api, fix environment import

--- src/app/supabase-client/supabase-client.ts
+++ src/app/supabase-client/supabase-client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from 'schema';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 export const createSupabaseClient = () => {
     return createClient<Database>(environment.subabaseUrl, environment.anonkey);
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
+    production: false,
     subabaseUrl: 'http://localhost:54321',
     anonkey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0',
